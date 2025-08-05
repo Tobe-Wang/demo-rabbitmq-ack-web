a13b5fe7a4d8bdec1dfbff858b549ef824ac649d
Tobe-Wang <hzbeq@qq.com>
2025-08-05 14:02:36 +0800

    feat(RabbitMQ): 增加用户消息发送和接收功能- 新增 SysUser 消息发送和接收方法 - 添加 sendSysUserMsg 控制器方法- 实现 SysUser 消息的发送和接收逻辑 -增加日志记录和消息确认功能

--- src/main/java/cn/zhaofd/demorabbitmqackweb/demo/service/RabbitMQService.java
+++ src/main/java/cn/zhaofd/demorabbitmqackweb/demo/service/RabbitMQService.java
@@ -1,9 +1,11 @@
 package cn.zhaofd.demorabbitmqackweb.demo.service;
 
+import cn.zhaofd.demorabbitmqackweb.demo.dto.SysUser;
 import com.rabbitmq.client.Channel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.AmqpHeaders;
@@ -21,11 +23,13 @@ public class RabbitMQService {
     private static final Logger logger = LoggerFactory.getLogger(RabbitMQService.class);
     private final RabbitTemplate rabbitTemplate;
     private final Binding logBinding;
+    private final Queue sysUserQueue;
 
     @Autowired
-    public RabbitMQService(RabbitTemplate rabbitTemplate, Binding logBinding) {
+    public RabbitMQService(RabbitTemplate rabbitTemplate, Binding logBinding, Queue sysUserQueue) {
         this.rabbitTemplate = rabbitTemplate;
         this.logBinding = logBinding;
+        this.sysUserQueue = sysUserQueue;
     }
 
     /**
@@ -35,6 +39,15 @@ public class RabbitMQService {
      */
     public void sendMessage(String message) {
         rabbitTemplate.convertAndSend(logBinding.getExchange(), logBinding.getRoutingKey(), message);
+    }
+
+    /**
+     * 发送消息
+     *
+     * @param sysUser 用户
+     */
+    public void sendMessage(SysUser sysUser) {
+        rabbitTemplate.convertAndSend(sysUserQueue.getName(), sysUser);
     }
 
     /**
@@ -48,6 +61,27 @@ public class RabbitMQService {
     public void receiveMessage(String message, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag) throws IOException {
         try {
             logger.info("Received <{}>", message);
+            // 确认消息
+            // 第2个参数：表示是否批量确认消息，true确认所有比deliveryTag更小或相等的未确认消息；false仅确认当前指定的deliveryTag消息
+            channel.basicAck(tag, false);
+        } catch (IOException e) {
+            // 拒绝并重入队列
+            // 第2个参数：表示是否批量拒绝消息，true拒绝所有比deliveryTag更小或相等的未确认消息；false仅拒绝当前指定的deliveryTag消息
+            // 第3个参数：表示是否重新入队列
+            channel.basicNack(tag, false, true);
+            throw e;
+        }
+    }
+
+    /**
+     * 接收消息并ACK
+     *
+     * @param sysUser 用户
+     */
+    @RabbitListener(queues = "sysUser-queue")
+    public void receiveMessage(SysUser sysUser, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag) throws IOException {
+        try {
+            logger.info("Received SysUser <id={},name={},regtime={}>", sysUser.getId(), sysUser.getName(), sysUser.getRegtime());
             // 确认消息
             // 第2个参数：表示是否批量确认消息，true确认所有比deliveryTag更小或相等的未确认消息；false仅确认当前指定的deliveryTag消息
             channel.basicAck(tag, false);
--- src/main/java/cn/zhaofd/demorabbitmqackweb/demo/web/RabbitMQController.java
+++ src/main/java/cn/zhaofd/demorabbitmqackweb/demo/web/RabbitMQController.java
@@ -1,10 +1,13 @@
 package cn.zhaofd.demorabbitmqackweb.demo.web;
 
+import cn.zhaofd.demorabbitmqackweb.demo.dto.SysUser;
 import cn.zhaofd.demorabbitmqackweb.demo.service.RabbitMQService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 /**
  * RabbitMQ应用示例
@@ -27,5 +30,18 @@ public class RabbitMQController {
     @GetMapping("/sendMessage")
     public void sendMessage(String message) {
         rabbitMQService.sendMessage(message);
+    }
+
+    /**
+     * 发送消息(用户)
+     */
+    @GetMapping("/sendSysUserMsg")
+    public void sendSysUserMsg() {
+        SysUser sysUser = new SysUser();
+        sysUser.setId(1);
+        sysUser.setName("张三");
+        sysUser.setRegtime(LocalDateTime.now());
+
+        rabbitMQService.sendMessage(sysUser);
     }
 }
