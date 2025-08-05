01beb7e74dc8773332c2e09acd76121b0629aa12
Tobe-Wang <hzbeq@qq.com>
2025-08-05 14:01:22 +0800

    feat: 添加用户实体类 SysUser
    - 新增 SysUser 类，包含用户 ID、用户名和注册时间等属性
    - 实现 Serializable接口，用于对象序列化- 使用 Jackson 和 Spring 注解进行日期格式化

---
+++ src/main/java/cn/zhaofd/demorabbitmqackweb/demo/dto/SysUser.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2025. Tobe Wang
+ */
+
+package cn.zhaofd.demorabbitmqackweb.demo.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * 用户
+ */
+public class SysUser implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+
+    private String name;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") // Spring框架注解，用于前端到后端的数据绑定（HTTP请求参数->Java对象）
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") // Jackson库注解，用于JSON序列化和反序列化（Java对象<->JSON字符串）
+    private LocalDateTime regtime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getRegtime() {
+        return regtime;
+    }
+
+    public void setRegtime(LocalDateTime regtime) {
+        this.regtime = regtime;
+    }
+}
